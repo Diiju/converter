2f8c98a1e0bd30e4ddd8897c850340fa114527f7
Prayas Jain <prayas.jn24@gmail.com>
2021-06-27 10:51:34 +0530

    updated xd2dju

--- xd2dju.py
+++ xd2dju.py
@@ -1,4 +1,5 @@
 import os
+from os.path import isfile
 from zipfile import ZipFile, is_zipfile
 import shutil
 import json
@@ -9,17 +10,19 @@ artboards = {}
 pasteboard = {}
 projName = 'default'
 
+dji = {}
+
 def getPath(base:str, rel:str):
     '''joins path of base and rel with appropriate seperator'''
     rel = rel.replace('\\', os.sep).replace('/', os.sep)
     return os.path.join(base, rel)
 
-def parse(node:dict, path:str, name:str = ''):
+def parse(node:dict, path:str, name:list = []):
     '''flatten recursive dictionary into assets and flat dictionary pointers'''
     global assets, component, artboards, pasteboard, projName
     if 'path' in node:
         if 'artboard' in node['path']:
-            name = node['name']
+            name = [node['name'], node['path']]
         path = getPath(path, node['path'])
     if 'children' in node:
         for child in node['children']:
@@ -40,7 +43,8 @@ def parse(node:dict, path:str, name:str = ''):
                 elif 'pasteboard' in path:
                     pasteboard = node
                 else:   
-                    artboards[name] = node
+                    node['name'] = name[0]
+                    artboards[name[1]] = node
             elif 'interactions' in filetype:
                 pass
             elif 'sharing' in filetype:
@@ -50,9 +54,75 @@ def parse(node:dict, path:str, name:str = ''):
         elif 'dcx' in filetype:
             projName = node['name']
 
+def boardParser(node:dict):
+    '''
+    recursive function to parse different graphicTree objects into dji
+    format
+    '''
+    newNode = {}
+    nodeType = node['type']
+    newNode['id'] = node['id']
+    if 'name' in node:
+        newNode['name'] = node['name']
+    if 'visible' in node:
+        newNode['visible'] = node['visible']
+    else:
+        newNode['visible'] = True
+    if 'locked' in node:
+        newNode['locked'] = node['locked']
+    else:
+        newNode['locked'] = False
+    if nodeType in ['artboard', 'group']:
+        newNode['children'] = []
+        for child in node[nodeType]['children']:
+            newNode['children'].append(boardParser(child))
+    return newNode
+
+def saveDji(savePath:str):
+    '''Creates json+dji file while calling artboard parser'''
+    global dji, projName
+    for obj in artboards:
+        artboard = open(artboards[obj]['path'], 'r', encoding='utf-8').read()
+        artboard = json.loads(artboard)
+        artboard = artboard['children'][0]
+        artboard['name'] = artboards[obj]['name']
+        dji[artboard['name']] = boardParser(artboard)
+    outPath = getPath(savePath, projName)  + '.dji'
+    with open(outPath, "w") as outfile: 
+        json.dump(dji, outfile, indent=4)
+
+def saveImg(savePath:str):
+    '''
+    function to assign id based filename and move resources from
+    xd to dju
+    '''
+    savePath = getPath(savePath, 'assets')
+    os.makedirs(savePath)
+    for idx in assets:
+        src = assets[idx]['path']
+        dest = getPath(savePath, idx)
+        shutil.copy(src, dest)
+
+def save(savePath:str, filename:str):
+    '''
+    save function to create a zip+dju format with an asset folder
+    and a single json+dji file
+    '''
+    if os.path.exists(savePath):
+        shutil.rmtree(savePath)
+    saveImg(savePath)
+    saveDji(savePath)
+    outPath = filename.split('.xd')[0]
+    if os.path.exists(outPath+'.dju'):
+        os.remove(outPath+'.dju')
+    shutil.make_archive(outPath, 'zip', savePath)
+    shutil.move(outPath+'.zip', outPath+'.dju')
+    shutil.rmtree(savePath)
+
 def xd2dju(fpath:str):
     '''converts xd to dju given the relative file path'''
-    cache = '.' + fpath.split('/')[-1].split('\\')[-1]
+    filename = fpath.split('/')[-1].split('\\')[-1]
+    cache = '.' + filename
     if os.path.exists(cache):
         shutil.rmtree(cache)
     if is_zipfile(fpath):
@@ -61,6 +131,7 @@ def xd2dju(fpath:str):
         manifest = open(getPath(cache, 'manifest'), 'r', encoding='utf-8').read()
         manifest = json.loads(manifest)
         parse(manifest, cache)
+        save('_out'+cache, filename)
         shutil.rmtree(cache)
 
 xd2dju('pain.xd')
